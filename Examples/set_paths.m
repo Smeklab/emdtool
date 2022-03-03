060690acf1ee776538c66cfd94509f6cc86d7d5c
Antti Lehikoinen <antti@smeklab.com>
2022-03-03 09:13:40 +0200

    gitignore

---
+++ Examples/set_paths.m
@@ -0,0 +1,7 @@
+%set_paths Set EMDtool and examples path
+%
+% (c) 2022 Smeklab Ltd / Antti Lehikoinen. All rights reserved.
+
+addpath(genpath(emdtool_path));
+addpath(genpath('Analysis scripts'));
+addpath(genpath('Geometries'));
